83b18dc1049214dfe934c7d296e0ec7200b34ef8
8bitty <max@koeninger.org>
2023-11-06 17:03:42 -0600

    added random number and string generation for testing

--- main.cpp
+++ main.cpp
@@ -3,3 +3,27 @@
  Programming Fundementals 3
  Purpose: a first in, first out stack using a linked list
  **********************/
+
+#include "main.h"
+
+int main(){
+    srand(time(NULL));
+    
+    int ids[DATA_SIZE];
+    string strs[DATA_SIZE];
+    for (int i = 0; i < DATA_SIZE; i++){
+        ids[i] = rand() % MAXID;
+    }
+    for (int i = 0; i < DATA_SIZE; i++){
+        strs[i] = 32 + rand() % 95;
+    }
+    cout << "Displaying test data:" <<endl;
+    for (int i = 0; i < DATA_SIZE; i++){
+        cout << ids[i] << ": " << strs[i] <<endl;
+    }
+    LinkedList list1;
+    for (int i = 0; i < DATA_SIZE; i++){
+        cout << list1.addNode(ids[i], &strs[i]) <<endl;
+    }
+    return 0;
+}
--- main.h
+++ main.h
@@ -3,3 +3,21 @@
  Programming Fundementals 3
  Purpose: a first in, first out stack using a linked list
  **********************/
+
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdlib.h>
+#include <time.h>
+#include <iostream>
+#include <string>
+
+#include "fifo.h"
+
+#define MAXID 100
+#define DATA_SIZE 10
+
+using std::cout;
+using std::endl;
+
+#endif
